69d5fea8974865cc6b09fda3c21ef2801b9a69f3
github username <github email address>
2016-07-11 21:03:05 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,13 +1,13 @@
 function ContactController() {
     this.contacts = [{
         name: 'Bob',
-        phone: '0123458690'
+        phoneNumber: '0123458690'
     },{
         name: 'Tim',
-        phone: '3934203242'
+        phoneNumber: '3934203242'
     },{
         name: 'Ross',
-        phone: '0684059433'
+        phoneNumber: '0684059433'
     }];
 
     this.removeContact = function (index) {
@@ -17,8 +17,10 @@ function ContactController() {
     this.submitContact = function (){
       this.contacts.push({
         name: this.name,
-        phone: this.phone
+        phoneNumber: this.phoneNumber
       });
+        this.name = '';
+        this.phoneNumber = '';
     }
 }
 
